3ae3f09fe7ddce038c68acca739374e4793d210c
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-08-27 15:30:04 -0400

    tt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
+// next.config.js or next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        // Exclude source maps for chrome-aws-lambda
+        config.externals = [...config.externals, /chrome-aws-lambda/];
+        
+        // Include the necessary dependencies for Chromium
+        const modifiedRules = config.module.rules.map(rule => {
+          if (rule.oneOf) {
+            rule.oneOf.forEach((oneOf) => {
+              if (!Array.isArray(oneOf.use)) return;
+              oneOf.use.forEach((moduleLoader) => {
+                if (
+                  moduleLoader.loader.includes("css-loader") &&
+                  moduleLoader.options.modules
+                ) {
+                  moduleLoader.options.modules.auto = () => true;
+                }
+              });
+            });
+          }
+          return rule;
+        });
+  
+        config.module.rules = modifiedRules;
+      }
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
